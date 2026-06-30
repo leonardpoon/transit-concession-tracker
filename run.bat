5de864a32a657a3e2e1036e5fda167d4bb4ad13e
Leonard <leonardpoonkokwei@gmail.com>
2026-06-30 15:04:45 +0800

    updated run batch file to include new environment  variable settings for the application.

--- run.bat
+++ run.bat
@@ -8,11 +8,10 @@ echo   [1] Launch CMD app
 echo   [2] Launch Dashboard
 echo.
 set /p choice="Enter choice: "
-
 if "%choice%"=="1" (
-    "D:\personal-projects\venv\Scripts\python.exe" main.py
+    "%~dp0..\..\venv\Scripts\python.exe" main.py
 )
 if "%choice%"=="2" (
-    "D:\personal-projects\venv\Scripts\python.exe" -m streamlit run dashboard.py
+    "%~dp0..\..\venv\Scripts\python.exe" -m streamlit run dashboard.py
 )
 pause
